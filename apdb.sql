3313f69ff12a77ea9ba645a7acf74c462f823470
Dayaseh (DJ) <71980784+johndaya1@users.noreply.github.com>
2022-04-13 17:53:29 -0400

    Update apdb.sql

--- apdb.sql
+++ apdb.sql
@@ -125,7 +125,8 @@ CREATE TABLE apartmentUnit    /*priscilla*/
    SELECT e.name, e.eid
    FROM   employee e, maintenanceRequest m, worksOn w, certification c
    WHERE e.eid = w.eid AND 
-	 c.cert = electrical AND
+	 /*c.cert = electrical AND*/
+	 e.eid = c.eid AND
 	 e.mgrId = m.mgrId AND
 	 w.requestId = m.requestId
   ORDER BY eid;
