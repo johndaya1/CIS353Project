ceb45a08475398f652839bce25685b86cd99e0c7
jackaftershok <38927130+jackaftershok@users.noreply.github.com>
2022-04-14 18:29:13 -0400

    Update aptDB-Jack.sql

--- aptDB-Jack.sql
+++ aptDB-Jack.sql
@@ -1,4 +1,116 @@
-/*--------------------------------------------------
+SPOOL aptDB.txt
+SET ECHO ON
+
+
+--Authors: Dayaseh Johnson, Priscila Ontiveros, , ,
+
+
+-------------------------------------------
+
+
+DROP TABLE resident CASCADE CONSTRAINTS;
+DROP TABLE apartmentUnit CASCADE CONSTRAINTS;
+DROP TABLE maintenanceRequest CASCADE CONSTRAINTS;
+DROP TABLE buildingManager CASCADE CONSTRAINTS;
+DROP TABLE employee CASCADE CONSTRAINTS;
+DROP TABLE worksOn CASCADE CONSTRAINTS;
+DROP TABLE certification CASCADE CONSTRAINTS;
+
+------------------
+
+CREATE TABLE resident    /* Priscilla*/
+(        
+  resID INTEGER PRIMARY KEY,
+  unitNum INTEGER NOT NULL,
+  collegeYear INTEGER NOT NULL,
+  name CHAR(50) NOT NULL,
+  SEX CHAR(1) NOT NULL,
+  major CHAR(30)
+  CONSTRAINT r1 CHECK(SEX IN ('female','male')),
+  CONSTRAINT r2 CHECK(major IN ('Computer Science'))
+);
+  
+------------------
+  
+CREATE TABLE apartmentUnit    /*priscilla*/
+(
+  roomNum INTEGER PRIMARY KEY,
+  style CHAR(12) NOT NULL,
+  condition CHAR(12) NOT NULL,
+  price INTEGER NOT NULL,
+  availability CHAR(12) NOT NULL,
+  CONSTRAINT a1 CHECK (availability IN ('open', 'not open')),
+  CONSTRAINT a2 CHECK (NOT (condition = 'poor' AND availability = 'open'))
+);
+
+------------------
+
+CREATE TABLE buildingManager    /*Grant*/
+(
+  mgrId INTEGER PRIMARY KEY,
+  isLandlord CHAR(3) NOT NULL,
+  name CHAR(32) NOT NULL,
+  pay INTEGER NOT NULL
+  /*TO DO: INSERT CONSTRAINTS*/
+  CONSTRAINT bm1 CHECK (NOT (pay < 55000) OR (pay > 100000)),
+  CONSTRAINT bm2 CHECK (isLandlord IN ('yes', 'no', 'Yes', 'No')),
+  CONSTRAINT bm3 CHECK (NOT (pay < 80000 AND isLandlord = 'yes'))
+);
+
+-------------------
+
+CREATE TABLE maintenanceRequest    /*Grant*/
+(
+  resId INTEGER,
+  day DATE,
+  maintenanceType CHAR(15),
+  mgrId INTEGER,
+  PRIMARY KEY (resId, day),
+  FOREIGN KEY (resId) REFERENCES resident(resId),
+  FOREIGN KEY (mgrId) REFERENCES buildingManager(mgrId),
+  /*TO DO: INSERT CONSTRAINTS*/
+  CONSTRAINT m1 CHECK (maintenanceType IN ('plumbing', 'electrical', 'mechanical', 'furniture'))
+);
+
+-------------------
+
+CREATE TABLE employee        /*DJ*/
+(
+  eId INTEGER PRIMARY KEY,
+  name CHAR(32) NOT NULL,
+  pay INTEGER,
+  mgrID INTEGER NOT NULL,
+  /*TO DO: INSERT CONTRAINTS*/
+  CONSTRAINT e1 CHECK (NOT (eId = mgrID)),
+  CONSTRAINT e2 CHECK (NOT (pay < 25000) OR (pay > 55000))
+);
+
+-------------------
+
+CREATE TABLE certification   /*DJ*/
+(
+  eId INTEGER,
+  cert CHAR(15),
+  FOREIGN KEY (eId) REFERENCES employee(eId),
+  /*TO DO: INSERT CONSTRAINTS*/
+  CONSTRAINT c1 CHECK (cert IN ('plumbing', 'electrical', 'mechanical', 'furniture'))
+);
+    
+-------------------
+
+CREATE TABLE worksOn      /*Jack*/
+(
+  eId INTEGER,
+  resId INTEGER,
+  day DATE,
+  hours INTEGER,
+  FOREIGN KEY (eId) REFERENCES employee(eId),
+  FOREIGN KEY (resId, day) REFERENCES maintenanceRequest(resId, day),
+  PRIMARY KEY(resId, day)
+);
+    
+
+    /*--------------------------------------------------
             Populate Database/IC Testing - Jack
     ----------------------------------------------------*/
     alter session set NLS_DATE_FORMAT = 'MM-DD-YY';
@@ -26,7 +138,7 @@
         -- bm1 (between 55000 and 100000 pay)
         insert into buildingManager values(999, 'No', 'Bob', 25000);
         -- bm2 (Landlord value constraint)
-        insert into buildingManager values(999, 'Y', 'Bob', 55000);
+        insert into buildingManager values(999, 'Maybe', 'Bob', 55000);
         -- bm3 (is NOT a Landlord and pay is higher than or equal to 80000)
         insert into buildingManager values(999, 'No', 'Bob', 100000);
 
@@ -63,5 +175,51 @@
     insert into certification values (24, 'electrical');
     insert into certification values (30, 'electrical');
         --## IC Testing ##--
-        -- c1 (check for a valid maintenance type)
+        -- 11 (check for a valid maintenance type)
         insert into certification values (36, 'flooring');
+        
+    ---------------------------------------------------
+
+    --QUERY 1
+    /* select the name and employee ID of each employee who has a certification matching the maintenance type of a maintenance request managed by their own manager*/
+    SELECT e.name, e.eid
+    FROM   employee e, maintenanceRequest m, worksOn w, certification c
+    WHERE e.eid = w.eid AND 
+        c.cert = m.maintenanceType AND
+        e.eid = c.eid AND
+        e.mgrId = m.mgrId AND
+        w.requestId = m.requestId
+    ORDER BY eid;
+        
+    --QUERY 2
+    /*select find pairs of students that are the same sex and 1 year apart in school. select the name, sex, and current year*/
+    SELECT DISTINCT r1.name, r2.name, r1.sex, r2.sex, r1.collegeYear, r2.collegeYear
+    FROM resident r1, resident r2
+    WHERE r1.sex = r2.sex AND
+        r1.collegeYear = r2.collegeYear + 1 AND
+        (NOT r1.name = r2.name)
+    ORDER BY r1.collegeYear ASC;
+
+    --QUERY 7
+    /*Find the resId of every student that is past their 1st year and has not submitted a maintenance request*/	   
+    SELECT DISTINCT r1.resId
+    FROM resident r1
+    WHERE collegeYear > 1 AND
+        r1.resId NOT IN (SELECT r2.resId
+                FROM maintenanceRequest r2);
+
+    --QUERY 8
+    /*find the resId every resident that has submitted a maintenance request that was electrical*/
+    SELECT DISTINCT mr1.resId
+    FROM maintenanceRequest mr1
+    WHERE NOT EXISTS ((SELECT mr2.maintenanceType
+                FROM maintenanceRequest mr2
+                WHERE maintenanceType = 'electrical')
+                MINUS
+                (SELECT maintenanceType 
+                FROM maintenanceRequest mr3
+                WHERE mr1.resID = mr3.resID AND
+                    mr3.maintenanceType = 'electrical'
+                ));
+
+    COMMIT;
