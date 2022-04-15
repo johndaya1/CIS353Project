24cad4a79b9831405bb46a0f1e09d3c45dfd0d55
jackaftershok <38927130+jackaftershok@users.noreply.github.com>
2022-04-14 22:56:06 -0400

    Update apDJ.sql

--- apDJ.sql
+++ apDJ.sql
@@ -45,18 +45,20 @@ CREATE TABLE apartmentUnit    /*priscilla*/
   CONSTRAINT a4 CHECK (condition IN ('poor', 'ok', 'great', 'perfect'))
  );
    ------------------
+
    CREATE TABLE buildingManager    /*Grant*/
   (
     mgrId INTEGER PRIMARY KEY,
     isLandlord CHAR(3) NOT NULL,
     name CHAR(32) NOT NULL,
     pay INTEGER NOT NULL
-    /*TO DO: INSERT CONSTRAINTS*/
     CONSTRAINT bm1 CHECK (NOT (pay < 55000) OR (pay > 100000)),
-    CONSTRAINT bm3 CHECK (isLandlord IN ('yes', 'no', 'Yes', 'No')),
-    CONSTRAINT bm4 CHECK (NOT (pay < 100000 AND isLandlord = 'yes'))
+    CONSTRAINT bm2 CHECK (isLandlord IN ('yes', 'no', 'Yes', 'No')),
+    CONSTRAINT bm3 CHECK (NOT (pay < 80000 AND isLandlord = 'yes'))
    );
+
    -------------------
+
  CREATE TABLE maintenanceRequest    /*Grant*/
  (
    resId INTEGER,
@@ -77,7 +79,7 @@ CREATE TABLE apartmentUnit    /*priscilla*/
      pay INTEGER,
      mgrID INTEGER NOT NULL,
      CONSTRAINT e1 CHECK (NOT (eId = mgrID)),
-     CONSTRAINT e2 CHECK (NOT (pay > 55000) OR (pay < 25000))
+     CONSTRAINT e2 CHECK (NOT (pay < 25000) OR (pay > 55000))
     );
      ----------------
    CREATE TABLE certification   /*DJ*/
@@ -98,7 +100,7 @@ CREATE TABLE apartmentUnit    /*priscilla*/
       FOREIGN KEY (eId) REFERENCES employee(eId),
       FOREIGN KEY (resId, day) REFERENCES maintenanceRequest(resId, day),
       PRIMARY KEY(resId, day),
-      CONSTRAINT w1 CHECK(NOT (hours < 0) OR (hours > 50));
+      CONSTRAINT w1 CHECK(NOT (hours < 0) OR (hours > 50))
      );
     
    /*--------------------------------------------------       Jack
@@ -106,136 +108,112 @@ CREATE TABLE apartmentUnit    /*priscilla*/
    ----------------------------------------------------*/
    
    alter session set NLS_DATE_FORMAT = 'MM-DD-YY';
-   
-   
-   insert into resident values (1234, 01, 2, 'John Doe', 'M', 'Computer Science');
-   insert into resident values (1235, 02, 3, 'Jane Doe', 'F', 'Computer Science');
-   insert into resident values (1236, 03, 4, 'Beyonce Knowles', 'F', 'Computer Science');
-   insert into resident values (1237, 04, 1, 'Michael Jackson', 'M', 'Computer Science');
-   insert into resident values (1238, 05, 2, 'Elvis Presley', 'M', 'Computer Science');
-   
-  --## IC Testing ##-- WORKS
+    insert into resident values (1234, 01, 2, 'John Doe', 'M', 'Computer Science');
+    insert into resident values (1235, 02, 3, 'Jane Doe', 'F', 'Computer Science');
+    insert into resident values (1236, 03, 4, 'Beyonce Knowles', 'F', 'Computer Science');
+    insert into resident values (1237, 04, 1, 'Michael Jackson', 'M', 'Computer Science');
+    insert into resident values (1238, 05, 2, 'Elvis Presley', 'M', 'Computer Science');
+        --## IC Testing ##--
         -- r1 (Gender constraint)
         insert into resident values (0001, 10, 1, 'Mike Mike', 'm', 'Computer Science');
-        insert into resident values (0001, 10, 1, 'Mike Mike', 'f', 'Computer Science');
         -- r2 (Major constraint)
         insert into resident values (0001, 10, 1, 'Mike Mike', 'M', 'Business');
-        insert into resident values (0001, 10, 1, 'Mike Mike', 'F', 'Gender Studies');
------------------------------------------------------------------------------------------------------------------
-   
-   insert into apartmentUnit values(01, 'style 1', 'great', 1500, 'not open');
-    insert into apartmentUnit values(02, 'style 2', 'ok', 1200, 'not open');
-    insert into apartmentUnit values(03, 'style 3', 'great', 1500, 'not open');
-    insert into apartmentUnit values(04, 'style 4', 'perfect', 1700, 'not open');
-    insert into apartmentUnit values(05, 'style 5', 'ok', 2000, 'not open');
-    insert into apartmentUnit values(06, 'style 6', 'great', 2000, 'not open');
-    --testing a1
-    insert into apartmentUnit values(20, 'style 3', 'great', 1500, 'closed');
-    --testing a2 
-    insert into apartmentUnit values(21, 'style 1', 'poor', 1500, 'open');
-    --testing a3
-    insert into apartmentUnit values(22, 'style 12', 'great', 1500, 'not open');
-    --testing a4
-    insert into apartmentUnit values(23, 'style 1', 'eh', 1500, 'not open');
-    insert into apartmentUnit values(24, 'style 1', 'eh', 1500, 'not open');
------------------------------------------------------------------------------------------------------------------------------------
-   insert into buildingManager values(123, 'No', 'Bill', 55000);
-   insert into buildingManager values(124, 'No', 'Tammy', 55000);
-   --## IC Testing ##-- WORKS
+        -- r3 (collegeYear cannot be more than 6)
+        insert into resident values (0001, 10, 7, 'Mike Mike', 'M', 'Business');
+
+    -------------------
+
+    insert into apartmentUnit values(01, 'Style 1', 'great', 1500, 'not open');
+    insert into apartmentUnit values(02, 'Style 2', 'ok', 1200, 'not open');
+    insert into apartmentUnit values(03, 'Style 3', 'great', 1500, 'not open');
+    insert into apartmentUnit values(04, 'Style 4', 'perfect', 1700, 'not open');
+    insert into apartmentUnit values(05, 'Style 5', 'ok', 2000, 'not open');
+    insert into apartmentUnit values(06, 'Style 6', 'great', 2200, 'not open');
+        --## IC Testing ##--
+        -- a1
+        insert into apartmentUnit values(20, 'style 3', 'great', '1500', 'closed');
+        -- a2 
+        insert into apartmentUnit values(21, 'style 1', 'poor', '1500', 'open');
+        -- a3
+        insert into apartmentUnit values(22, 'style 12', 'great', '1500', 'not open');
+        -- a4
+        insert into apartmentUnit values(23, 'style 1', 'eh', '1500', 'not open');
+        insert into apartmentUnit values(24, 'style 1', 'eh', '1500', 'not open');
+
+    -------------------
+
+    insert into buildingManager values(123, 'No', 'Bill', 55000);
+    insert into buildingManager values(124, 'Yes', 'Tammy', 55000);
+        --## IC Testing ##-- WORKS
         -- bm1 (between 55000 and 100000 pay)
         insert into buildingManager values(999, 'No', 'Bob', 25000);
         -- bm2 (Landlord value constraint)
         insert into buildingManager values(999, '??', 'Bob', 55000);
         -- bm3 (is NOT a Landlord and pay is higher than or equal to 80000)
-        --NOT WORKING
         insert into buildingManager values(999, 'Yes', 'Bob', 10000);
-------------------------------------------------------------------------------------------------------------------------
-   insert into employee values (12, 'Tom', 30000, 123);
-   insert into employee values (18, 'David', 30000, 123);
-   insert into employee values (24, 'John', 30000, 123);
-   insert into employee values (30, 'Guy', 30000, 124);
-   --## IC Testing ##--
+
+    -------------------
+
+    insert into employee values (12, 'Tom', 30000, 123);
+    insert into employee values (18, 'David', 30000, 123);
+    insert into employee values (24, 'John', 30000, 123);
+    insert into employee values (30, 'Guy', 30000, 124);
+        --## IC Testing ##--
         -- e1 (check if employee is not a manager)
-        --NOT WORKING
         insert into employee values (36, 'Timothy', 30000, 36);
         -- e2 (check if pay is not less than 25000 and higher than 55000)
         insert into employee values (36, 'Timothy', 10000, 123);
- --------------------------------------------------------------------------------------------------------------------------
-   insert into maintenanceRequest values (1234, '01-01-22', 'electrical', 123);
-   insert into maintenanceRequest values (1234, '03-29-21', 'electrical', 124);
-   insert into maintenanceRequest values (1235, '03-05-22', 'plumbing', 124);
-   insert into maintenanceRequest values (1240, '01-01-22', 'furniture', 124);
-   --## IC Testing ##--
+
+    -------------------
+
+    insert into maintenanceRequest values (1234, '01-01-22', 'electrical', 123);
+    insert into maintenanceRequest values (1234, '03-29-21', 'electrical', 124);
+    insert into maintenanceRequest values (1235, '03-05-22', 'plumbing', 124);
+    insert into maintenanceRequest values (1236, '02-07-21', 'furniture', 124);
+        --## IC Testing ##--
         -- m1 (check for a valid maintenance type)
         insert into maintenanceRequest values (1235, '02-04-22', 'flooring', 124);
-        -- Foreign Key
+        -- resId Foreign Key
         insert into maintenanceRequest values (1000, '01-01-22', 'furniture', 124);
-------------------------------------------------------------------------------------------------------------------
-   insert into worksOn values (12, 1234, '01-01-22', 5);
-   --## IC Testing ##--
+        -- mgrId Foreign Key
+        insert into maintenanceRequest values (1235, '01-01-22', 'furniture', 100);
+
+    -------------------
+    
+    insert into certification values (12, 'electrical');
+    insert into certification values (18, 'plumbing');
+    insert into certification values (24, 'electrical');
+    insert into certification values (30, 'electrical');
+        --## IC Testing ##--
+        -- c1 (check for a valid maintenance type)
+        insert into certification values (30, 'flooring');
+        -- eId Foreign Key
+        insert into certification values (36, 'electrical');
+    
+    -------------------
+
+    insert into worksOn values (12, 1234, '01-01-22', 5);
+        --## IC Testing ##--
         -- eId Foreign Key
         insert into worksOn values (1, 1234, '01-01-22', 5);
         -- resId, day Foreign Key
         insert into worksOn values (12, 1000, '01-02-21', 5);
         -- w1 (checks if hours are below 0 or above 50)
         insert into worksOn values (12, 1234, '01-01-22', -1);
------------------------------------------------------------------------------------------------------
-   insert into certification values (12, 'electrical');
-   insert into certification values (18, 'plumbing');
-   insert into certification values (24, 'electrical');
-   insert into certification values (30, 'electrical');
-   --## IC Testing ##--
-        -- c1 (check for a valid maintenance type)
-        insert into certification values (30, 'flooring');
-        -- eId Foreign Key
-        insert into certification values (36, 'electrical');	   
-   --QUERY 1
-   /* select the name and employee ID of each employee who has a certification matching the maintenance type of a maintenance request managed by their own manager*/
-   SELECT e.name, e.eid
-   FROM   employee e, maintenanceRequest m, worksOn w, certification c
-   WHERE e.eid = w.eid AND 
-	 c.cert = m.maintenanceType AND
-	 e.eid = c.eid AND
-	 e.mgrId = m.mgrId AND
-	 w.day = m.day
-  ORDER BY eid;
-	   
-  --QUERY 2
-  /*find pairs of students that are the same sex and 1 year apart in school. Get their name, sex, and current year*/
-  SELECT DISTINCT r1.name, r2.name, r1.sex, r2.sex, r1.collegeYear, r2.collegeYear
-  FROM resident r1, resident r2
-  WHERE r1.sex = r2.sex AND
-  	r1.collegeYear = r2.collegeYear + 1 AND
-	(NOT r1.name = r2.name)
-  ORDER BY r1.collegeYear ASC;
+        
+    ---------------------------------------------------
   
-	   --QUERY 8
-  /*find the resId of every resident that has submitted a maintenance request that was electrical*/
-  SELECT DISTINCT mr1.resId
-  FROM maintenanceRequest mr1
-  WHERE NOT EXISTS ((SELECT mr2.maintenanceType
-		     FROM maintenanceRequest mr2
-		     WHERE maintenanceType = 'electrical')
-		    MINUS
-		    (SELECT maintenanceType 
-		     FROM maintenanceRequest mr3
-		     WHERE mr1.resID = mr3.resID AND
-		     	   mr3.maintenanceType = 'electrical'
-		     ));
-	   --QUERY 7
-/*Find the resId of every student that is past their 1st year and has not submitted a maintenance request*/	   
-SELECT DISTINCT r.resId
+-- QUERY 3
+/*Find the resDd's and names of residents whose year is 3 and is a female.*/
+SELECT r.resId, r.name
 FROM resident r
-WHERE collegeYear > 1 AND
-      r.resId NOT IN (SELECT m.resId
-		       FROM maintenanceRequest m);
-	
-  --Query 9
-  /*find the name, resident Id, and unit number of every resident. Also show their maintenance requests*/
-  SELECT DISTINCT r.name, r.resId, r.unitNum
-  FROM residents r
-  LEFT OUTER JOIN maintenanceRequests m
-  ON r.resId = m.resId
-  ORDER BY r.resId
-    
-    COMMIT;
+WHERE collegeYear = 3
+MINUS
+SELECT r.resId, r.name
+FROM resident r
+WHERE sex = 'F';
+
+-- QUERY 4
+/* Select the highest paid employee from the employee table*/
+SELECT MAX(e.pay)
+FROM employee e;
