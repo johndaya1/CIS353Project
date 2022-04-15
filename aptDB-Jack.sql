9d2d15a20dba8ef9d9978f936ed9400e74119d89
jackaftershok <38927130+jackaftershok@users.noreply.github.com>
2022-04-14 22:26:27 -0400

    Update aptDB-Jack.sql

--- aptDB-Jack.sql
+++ aptDB-Jack.sql
@@ -25,8 +25,9 @@ CREATE TABLE resident (         /* Priscilla*/
   name CHAR(50) NOT NULL,
   sex CHAR(1) NOT NULL,
   major CHAR(30),
-  CONSTRAINT r1 CHECK(sex in ('M', 'F')),
-  CONSTRAINT r2 CHECK(major in('Computer Science'))
+  CONSTRAINT r1 CHECK (sex in ('M', 'F')),
+  CONSTRAINT r2 CHECK (major in('Computer Science')),
+  CONSTRAINT r3 CHECK (NOT(collegeYear > 6))
  );
   
   ------------------
@@ -39,8 +40,9 @@ CREATE TABLE apartmentUnit    /*priscilla*/
   price INTEGER NOT NULL,
   availability CHAR(12) NOT NULL,
   CONSTRAINT a1 CHECK (availability IN ('open', 'not open')),
-  CONSTRAINT a2 CHECK (condition IN ('poor', 'mediocre', 'good')),
-  CONSTRAINT a3 CHECK (style IN ('Style 1', 'Style 2', 'Style 3', 'Style 4','Style 5', 'Style 6'))
+  CONSTRAINT a2 CHECK (NOT (condition = 'poor' AND availability = 'open')),
+  CONSTRAINT a3 CHECK (style IN ('Style 1', 'Style 2', 'Style 3', 'Style 4','Style 5', 'Style 6')),
+  CONSTRAINT a4 CHECK (condition IN ('poor', 'ok', 'great', 'perfect'))
  );
    ------------------
 
@@ -67,7 +69,6 @@ CREATE TABLE apartmentUnit    /*priscilla*/
    PRIMARY KEY (resId, day),
    FOREIGN KEY (resId) REFERENCES resident(resId),
    FOREIGN KEY (mgrId) REFERENCES buildingManager(mgrId),
-  /*TO DO: INSERT CONSTRAINTS*/
    CONSTRAINT m1 CHECK (maintenanceType IN ('plumbing', 'electrical', 'mechanical', 'furniture'))
   );
    ---------------------
@@ -78,7 +79,6 @@ CREATE TABLE apartmentUnit    /*priscilla*/
      name CHAR(32) NOT NULL,
      pay INTEGER,
      mgrID INTEGER NOT NULL,
-     /*TO DO: INSERT CONTRAINTS*/
      CONSTRAINT e1 CHECK (NOT (eId = mgrID)),
      CONSTRAINT e2 CHECK (NOT (pay < 25000) OR (pay > 55000))
     );
@@ -88,7 +88,6 @@ CREATE TABLE apartmentUnit    /*priscilla*/
      eId INTEGER,
      cert CHAR(15),
      FOREIGN KEY (eId) REFERENCES employee(eId),
-     /*TO DO: INSERT CONSTRAINTS*/
      CONSTRAINT c1 CHECK (cert IN ('plumbing', 'electrical', 'mechanical', 'furniture'))
     );
      
@@ -101,7 +100,8 @@ CREATE TABLE apartmentUnit    /*priscilla*/
       hours INTEGER,
       FOREIGN KEY (eId) REFERENCES employee(eId),
       FOREIGN KEY (resId, day) REFERENCES maintenanceRequest(resId, day),
-      PRIMARY KEY (resId, day)
+      PRIMARY KEY(resId, day),
+      CONSTRAINT w1 CHECK(NOT (hours < 0) OR (hours > 50));
      );
     
    /*--------------------------------------------------       Jack
@@ -114,7 +114,7 @@ CREATE TABLE apartmentUnit    /*priscilla*/
     insert into resident values (1236, 03, 4, 'Beyonce Knowles', 'F', 'Computer Science');
     insert into resident values (1237, 04, 1, 'Michael Jackson', 'M', 'Computer Science');
     insert into resident values (1238, 05, 2, 'Elvis Presley', 'M', 'Computer Science');
-        --## IC Testing ##--
+        --## IC Testing ##-- WORKS
         -- r1 (Gender constraint)
         insert into resident values (0001, 10, 1, 'Mike Mike', 'm', 'Computer Science');
         insert into resident values (0001, 10, 1, 'Mike Mike', 'f', 'Computer Science');
@@ -126,12 +126,13 @@ CREATE TABLE apartmentUnit    /*priscilla*/
 
     insert into buildingManager values(123, 'No', 'Bill', 55000);
     insert into buildingManager values(124, 'Yes', 'Tammy', 55000);
-        --## IC Testing ##--
+        --## IC Testing ##-- WORKS
         -- bm1 (between 55000 and 100000 pay)
         insert into buildingManager values(999, 'No', 'Bob', 25000);
         -- bm2 (Landlord value constraint)
         insert into buildingManager values(999, '??', 'Bob', 55000);
         -- bm3 (is NOT a Landlord and pay is higher than or equal to 80000)
+        --NOT WORKING
         insert into buildingManager values(999, 'No', 'Bob', 100000);
 
     -------------------
@@ -142,6 +143,7 @@ CREATE TABLE apartmentUnit    /*priscilla*/
     insert into employee values (30, 'Guy', 30000, 124);
         --## IC Testing ##--
         -- e1 (check if employee is not a manager)
+        --NOT WORKING
         insert into employee values (36, 'Timothy', 30000, 123);
         --e2 (check if pay is not less than 25000 and higher than 55000)
         insert into employee values (36, 'Timothy', 10000, 123);
@@ -169,7 +171,7 @@ CREATE TABLE apartmentUnit    /*priscilla*/
     insert into certification values (24, 'electrical');
     insert into certification values (30, 'electrical');
         --## IC Testing ##--
-        -- 11 (check for a valid maintenance type)
+        -- c1 (check for a valid maintenance type)
         insert into certification values (36, 'flooring');
         
     ---------------------------------------------------
